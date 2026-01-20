ccff0fb4152821edfce931faebd9b8bced18dd23
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-20 09:07:32 +0100

    refact

---
+++ vue/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+
+        },
+        extend: {},
+
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
