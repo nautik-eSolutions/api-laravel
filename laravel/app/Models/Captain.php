7665bf1acda55043861926177440c1dcccfe4936
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-21 20:12:25 +0100

    Implementación de Relación Belongs to en Owner y Captain

--- laravel/app/Models/Captain.php
+++ laravel/app/Models/Captain.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Captain extends Model
 {
-    //
+    protected $table = 'captain';
+
+
+    protected $fillable = [
+        'navigation_license'=>'string'
+    ];
+
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- laravel/app/Models/Owner.php
+++ laravel/app/Models/Owner.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Owner extends Model
 {
-    //
+    protected $table = 'owner';
+
+    public function person() : BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
+
 }
