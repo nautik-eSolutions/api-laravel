f6f9c881235d98f47dd61e76d8faf9a6a9a07d7d
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-22 08:49:03 +0100

    Implementación de los métodos en el controller de owner

--- laravel/app/Http/Controllers/OwnerController.php
+++ laravel/app/Http/Controllers/OwnerController.php
@@ -25,12 +25,22 @@ class OwnerController extends Controller
     {
      $person = Person::find($id);
 
+        if (!$person) {
+            $data = [
+                'message' => "Person not found",
+                'status' => 400
+            ];
+
+            return response()->json($data, 404);
+
+        }
+
      $owner = new Owner();
+
      $person->owner()->save($owner);
 
      $owner->push();
-
-        $data = [
+     $data = [
             'owner'=>$owner,
             'status'=>201
         ];
@@ -42,16 +52,27 @@ class OwnerController extends Controller
 
 
 
-    public function show(Owner $owner)
+    public function show(String $id)
     {
-        //
-    }
+        $owner = Owner::find($id);
+
+        if (!$owner){
+            $data = [
+                'message'=>"Owner not found",
+                "status"=>404
+            ];
+            return response()->json($data,404);
+        }
+        $data = [
+          'owner'=>$owner,
+          'status'=>200
+        ];
+
+        return response()->json($data,200);
 
 
-    public function edit(Owner $owner)
-    {
-        //
     }
+
 
 
     public function update(Request $request, Owner $owner)
@@ -60,8 +81,15 @@ class OwnerController extends Controller
     }
 
 
-    public function destroy(Owner $owner)
+    public function destroy(String $id)
     {
-        //
+        $owner =  Owner::find($id);
+
+        $owner->delete();
+        $data=[
+          'message'=>'The owner has been deleted'
+        ];
+
+
     }
 }
