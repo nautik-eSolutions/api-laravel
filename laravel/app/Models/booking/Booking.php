d1bab3c3c2cf10041b17d10fda58bea1f8688327
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-28 07:56:59 +0100

    Creación del modelo booking

---
+++ laravel/app/Models/booking/Booking.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\booking;
+
+use App\Models\boats\Boat;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Booking extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'booking';
+
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'total_cost',
+        'payment_method',
+        'boat_id',
+    ];
+
+    public function boat(): BelongsTo
+    {
+        return $this->belongsTo(Boat::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+        ];
+    }
+}
