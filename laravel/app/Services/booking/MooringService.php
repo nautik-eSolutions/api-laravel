e20481b0a91b1aef2cc99321776458d3691ce889
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-30 15:04:27 +0100

    Implementación del método para ver los amarres disponible en puerto por fechas

--- laravel/app/Services/booking/MooringService.php
+++ laravel/app/Services/booking/MooringService.php
@@ -7,6 +7,7 @@ use App\Models\ports\Mooring;
 use App\Models\ports\Port;
 use Date;
 use Dflydev\DotAccessData\Data;
+use function PHPUnit\Framework\isFalse;
 
 class MooringService
 {
@@ -31,71 +32,44 @@ class MooringService
         foreach ($mooringsCategoriesCollection as $mooringsCategories) {
             foreach ($mooringsCategories as $mooringsCategory) {
                 foreach ($mooringsCategory->moorings as $mooring) {
-                    array_push($moorings,$mooring);
+                    $moorings[] = $mooring;
                 }
             }
         }
+
 
         return $moorings;
     }
 
 
-    public function showAvailableMooringsByPortAndDates(int $portId, $startDate,  $endDate)
+    public function showAvailableMooringsByPortAndDates(int $portId, $startDate, $endDate)
     {
-        $startDate = date_create_from_format("Y-m-d",$startDate);
-        $endDate = date_create_from_format("Y-m-d",$endDate);
+        $startDate = date_create_from_format("Y-m-d", $startDate);
+        $endDate = date_create_from_format("Y-m-d", $endDate);
 
         $moorings = $this->showMooringsByPort($portId);
-
-        $bookingsAvailable = [];
         $availableMoorings = [];
-
         $bookings = [];
-        foreach ($moorings as $mooring){
-            $bookings = $mooring->bookings
-                ->where('start_date','>=',$startDate)
-                ->where('end_date','<=',$endDate);
 
-            if ($bookings->all()){
-               $bookingsAvailable[]=$bookings->all();
+        foreach ($moorings as $mooring) {
+            $bookings[] = $mooring->bookings
+                ->search(
+                    function (Booking $booking) use ($startDate, $endDate) {
+                        return ($booking->start_date < $endDate) && ($booking->end_date > $startDate);
+                    });
+        }
+
+
+        for ($i = 0; $i < sizeof($bookings); $i++) {
+
+            if ($bookings[$i]===false){
+                $availableMoorings[] = $moorings[$i];
             }
         }
 
 
-
-        $mooring = Mooring::find(3);
-
-
-
-
-
-        dd($mooring->bookings->where('start_date','>=',$startDate)->where('end_date','<=',$endDate));
-
-        dd();
-
-
-
-
-
-        $test =  collect($moorings);
-        $test->filter(function(Mooring $mooring) use ($endDate, $startDate) {
-            return !($mooring->bookings->filter(function (Booking $booking) use ($startDate, $endDate) {
-                return  $booking->where('start_date','>=',$startDate)
-                    ->where('end_date','<=',$endDate);
-            }));
-        });
-
-        foreach ($bookingsAvailable as $booking){
-            foreach ($booking as $item){
-                $bookings [] = $item;
-            }
-        }
-
-
-        return $test;
+        return $availableMoorings;
     }
-
-
 
 
 }
