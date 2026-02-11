9b230b087dceb64c146243391031fa46a763b916
mlemrissani <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-02-11 13:41:01 +0100

    Refactoring de las rutas en api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -21,47 +21,11 @@ Route::controller(UserController::class)->group(function(){
    Route::delete('/users','destroy');
 })->middleware('auth:sanctum');
 
-
-
-/*
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{userId}', [UserController::class, 'show'])->middleware(UserExists::class);
-Route::patch('/users/{userId}', [UserController::class, 'update'])->middleware(UserExists::class);
-Route::delete('/users/{userId}', [UserController::class, 'destroy'])->middleware(UserExists::class);
-
-*/
-Route::post('/users/persons',[PersonController::class,'storeOwner'])->middleware('auth:sanctum');
-
-
-
-
-
-Route::get('/captains/{id}',[PersonController::class,'show']);
-Route::get('/owners/{id}',[PersonController::class,'show']);
-
-Route::get('/users/{userId}/captains',[PersonController::class,'showCaptainsByUser'])->middleware(UserExists::class);
-Route::get('/users/{userId}/owners',[PersonController::class,'showOwnersByUser'])->middleware(UserExists::class);
-Route::get('/users/{userId}/captains/{id}',[PersonController::class,'show'])->middleware(UserExists::class);
-Route::get('/users/{userId}/owners/{id}',[PersonController::class,'show'])->middleware(UserExists::class);
-
-Route::post('/users/{userId}/captains',[PersonController::class,'storeCaptain'])->middleware(UserExists::class);
-Route::patch('/users/{userId}/captains/{captainId}',[PersonController::class,'updateCaptain'])->middleware(UserExists::class);
-Route::delete('/users/{userId}/captains/{captainId}',[PersonController::class,'destroyCaptain'])->middleware(UserExists::class);
-Route::delete('/users/{userId}/owners/{ownerId}',[PersonController::class,'destroyOwner'])->middleware(UserExists::class);
-
-/*
-Route::get('/owners/{ownerId}/boats',[BoatController::class,'indexByOwner']);
-Route::get('/boats/{userId}/boats',[BoatController::class,'indexByUser']);
-Route::get('/boats/{id}',[BoatController::class,'show']);
-Route::post('/boats/{ownerId}',[BoatController::class,'store']);
-Route::patch('/boats/{boatId}',[BoatController::class,'update']);
-Route::delete('/boats/{boatId}',[BoatController::class,'destroy']);
-*/
-
 Route::controller(BoatController::class)->group(function(){
    Route::get('/boats','index');
    Route::post('/boats','store');
+   Route::patch('/boats/{id}','update');
+   Route::delete('boats/{id}','destroy');
 })->middleware('auth:sanctum');
 
 
